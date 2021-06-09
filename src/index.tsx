2299fef36a69aa3462e3781a8ed3d9e38fce2022
gabrielopeyemi <famosipe2010@gmail.com>
2021-06-09 23:51:50 +0100

    add encrytion

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,14 @@
 
 export const addToWispy = ( key: string, data : any) => {
   // add to localstorage without encoding
-  window.localStorage.setItem(key, JSON.stringify(data));
+  window.localStorage.setItem(key, btoa(JSON.stringify(data)));
   console.log('stored');
   return
 };
 
 export const getFromWispy = (key: any) => {
   let data: any = window.localStorage.getItem(key);
-  return JSON.parse(data);
-  
+  return JSON.parse(atob(data));
 }
 
 export const deleteFromWispy = (key: string) => {
